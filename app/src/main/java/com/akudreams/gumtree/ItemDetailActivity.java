8689ea6e8c95cf955fa512fd814414f4e78436c5
Jorge Perez Ruiz <akumayken@gmail.com>
2015-03-09 19:09:28 +0000

    Add game information in item view

--- app/src/main/java/com/akudreams/gumtree/ItemDetailActivity.java
+++ app/src/main/java/com/akudreams/gumtree/ItemDetailActivity.java
@@ -3,8 +3,12 @@ package com.akudreams.gumtree;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.akudreams.model.Item;
+import com.melnykov.fab.FloatingActionButton;
+import com.melnykov.fab.ObservableScrollView;
 import com.novoda.notils.caster.Views;
 
 public class ItemDetailActivity extends ActionBarActivity {
@@ -22,6 +26,31 @@ public class ItemDetailActivity extends ActionBarActivity {
         ViewPager imagesPager = Views.findById(this, R.id.images_pager);
         ImagesAdapter adapter = new ImagesAdapter(item.getImages(), getFragmentManager());
         imagesPager.setAdapter(adapter);
+
+        TextView titleView = Views.findById(this, R.id.title);
+        titleView.setText(item.getTitle());
+        TextView descriptionView = Views.findById(this, R.id.description);
+        descriptionView.setText(item.getDescription());
+        TextView postedDateView = Views.findById(this, R.id.date_posted);
+        postedDateView.setText(item.getDatePosted());
+        TextView locationTextView = Views.findById(this, R.id.location);
+        locationTextView.setText(item.getLocation());
+        TextView userNameView = Views.findById(this, R.id.user_name);
+        userNameView.setText(item.getOwner().getName());
+        TextView userCreationDateView = Views.findById(this, R.id.user_creation_date);
+        userCreationDateView.setText(item.getOwner().getAccountCreationDate());
+
+        ObservableScrollView scrollView = Views.findById(this, R.id.scroll_view);
+        FloatingActionButton floatingActionButton = Views.findById(this, R.id.fabbutton);
+        floatingActionButton.attachToScrollView(scrollView);
+        floatingActionButton.setOnClickListener(onContactUser);
     }
+
+    private final View.OnClickListener onContactUser = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+        }
+    };
+
 
 }
